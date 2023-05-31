bb0870bb9fcb3061ff728808c3024a12902cd18a
Maksym Zinchenko <maklaut98@gmail.com>
2023-05-31 15:34:40 -0500

    Added get products controller

--- src/main/java/com/example/zinware/controller/CategoryController.java
+++ src/main/java/com/example/zinware/controller/CategoryController.java
@@ -1,9 +1,11 @@
 package com.example.zinware.controller;
 
 import com.example.zinware.model.Category;
+import com.example.zinware.model.Product;
 import com.example.zinware.service.CategoryService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -24,8 +26,18 @@ public class CategoryController {
      * Get all categories
      * @return list of categories
      */
-    @GetMapping(path = "/categories")
+    @GetMapping(path = "/categories/")
     public List<Category> getAllCategories() {
         return categoryService.getAllCategories();
+    }
+
+    /**
+     * Get all products in a category
+     * @param categoryId id of the category
+     * @return list of products
+     */
+    @GetMapping(path = "/categories/{categoryId}/products/")
+    public List<Product> getCategoryProducts(@PathVariable Long categoryId) {
+        return categoryService.getCategoryProducts(categoryId);
     }
 }
