df40d4cba24838f1a06629354f266fed2a1b805c
Maksym Zinchenko <maklaut98@gmail.com>
2023-05-31 15:35:34 -0500

    Added get products from by category request to JPA

--- src/main/java/com/example/zinware/repository/ProductRepository.java
+++ src/main/java/com/example/zinware/repository/ProductRepository.java
@@ -4,5 +4,9 @@ import com.example.zinware.model.Category;
 import com.example.zinware.model.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface ProductRepository extends JpaRepository<Product, Long> {
+    Optional<List<Product>> findByCategory(Category category);
 }
