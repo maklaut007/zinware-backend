dc3408c4004d31001dfa7a01fa778c68012b2f6f
Maksym Zinchenko <maklaut98@gmail.com>
2023-05-31 11:45:53 -0500

    Created category controller

---
+++ src/main/java/com/example/zinware/controller/CategoryController.java
@@ -0,0 +1,10 @@
+package com.example.zinware.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(path = "/api/")
+public class CategoryController {
+
+}
