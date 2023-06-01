5a81b3a412244bc77342be2154bfc4d007a9ed55
Maksym Zinchenko <maklaut98@gmail.com>
2023-06-01 14:30:13 -0500

    Added getter and setter for product id

--- src/main/java/com/example/zinware/model/Product.java
+++ src/main/java/com/example/zinware/model/Product.java
@@ -76,4 +76,12 @@ public class Product {
     public void setCategory(Category category) {
         this.category = category;
     }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
 }
