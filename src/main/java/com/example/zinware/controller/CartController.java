10a8ffd3bc1cc2ee9a68e2da5374edb1841aec8f
Maksym Zinchenko <maklaut98@gmail.com>
2023-06-01 14:30:24 -0500

    Created Cart controller

---
+++ src/main/java/com/example/zinware/controller/CartController.java
@@ -0,0 +1,10 @@
+package com.example.zinware.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/cart")
+public class CartController {
+
+}
