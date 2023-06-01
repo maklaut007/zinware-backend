4fbe7e5880557a3303d0cc715f06d41702216c96
Maksym Zinchenko <maklaut98@gmail.com>
2023-06-01 17:21:28 -0500

    Added user registration to controller

--- src/main/java/com/example/zinware/controller/UserController.java
+++ src/main/java/com/example/zinware/controller/UserController.java
@@ -1,6 +1,8 @@
 package com.example.zinware.controller;
 
 import com.example.zinware.model.User;
+import com.example.zinware.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -10,7 +12,15 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping(path = "/auth/")
 public class UserController {
+    private UserService userService;
+    @Autowired
+    public void setUserService(UserService userService) {
+        this.userService = userService;
+    }
 
-
+    @PostMapping(path = "/register/")
+    public User registerUser(@RequestBody User user) {
+        return userService.registerUser(user);
+    }
 
 }
