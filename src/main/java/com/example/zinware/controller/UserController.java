33b3c65f0ad35f3f5f4f04bb7e812657752b6942
Maksym Zinchenko <maklaut98@gmail.com>
2023-06-01 17:35:48 -0500

    Minor fixes

--- src/main/java/com/example/zinware/controller/UserController.java
+++ src/main/java/com/example/zinware/controller/UserController.java
@@ -3,7 +3,6 @@ package com.example.zinware.controller;
 import com.example.zinware.model.User;
 import com.example.zinware.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
--- src/main/java/com/example/zinware/model/User.java
+++ src/main/java/com/example/zinware/model/User.java
@@ -3,7 +3,7 @@ package com.example.zinware.model;
 import javax.persistence.*;
 
 @Entity
-@Table(name = "user")
+@Table(name = "users")
 public class User {
     @Id
     @Column
