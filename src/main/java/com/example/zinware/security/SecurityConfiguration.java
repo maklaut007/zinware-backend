2ee1628aadd61e8a0dec21178c981e83d63e34a5
Maksym Zinchenko <maklaut98@gmail.com>
2023-06-01 16:43:58 -0500

    Filtered categories request from authorization

---
+++ src/main/java/com/example/zinware/security/SecurityConfiguration.java
@@ -0,0 +1,23 @@
+package com.example.zinware.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+public class SecurityConfiguration {
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.authorizeRequests().antMatchers("/api/categories/**")
+                .permitAll()
+                .anyRequest().authenticated()
+                .and().sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and().csrf().disable();
+        return http.build();
+    }
+}
