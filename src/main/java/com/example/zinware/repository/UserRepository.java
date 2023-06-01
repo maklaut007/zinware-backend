2781719f4ff0a757f7cd2e4a6e22a22ab1e482f5
Maksym Zinchenko <maklaut98@gmail.com>
2023-06-01 17:35:16 -0500

    Updated exists by email method

--- src/main/java/com/example/zinware/repository/UserRepository.java
+++ src/main/java/com/example/zinware/repository/UserRepository.java
@@ -5,5 +5,5 @@ import com.example.zinware.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User, Long> {
-    boolean existByEmail(String email);
+    boolean existsByEmail(String email);
 }
