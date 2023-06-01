f05fb62d33a47752a5f2af07038eee68021a303d
Maksym Zinchenko <maklaut98@gmail.com>
2023-06-01 09:25:23 -0500

    Updated picture addresses

--- src/main/java/com/example/zinware/model/Category.java
+++ src/main/java/com/example/zinware/model/Category.java
@@ -37,6 +37,14 @@ public class Category {
         this.picture = picture;
     }
 
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
     public String getName() {
         return name;
     }
--- src/main/java/com/example/zinware/seed/DataLoader.java
+++ src/main/java/com/example/zinware/seed/DataLoader.java
@@ -40,18 +40,18 @@ public class DataLoader implements CommandLineRunner {
      */
     private void loadUserData() {
         // Seed categories
-        Category category1 = new Category("Mouses", "PC Mouses", "");
-        Category category2 = new Category("Keyboards","PC Keyboards", "");
-        Category category3 = new Category("Headphones","PC Headphones", "");
+        Category category1 = new Category("Mouses", "PC Mouses", "https://media.discordapp.net/attachments/1112238965743964211/1113833439012081694/Allair_Gaming_mice_black_color_white_lights_dark_blue_backgroud_6584014f-6aff-438e-b1b9-c8fbd2368ff8.png?width=1298&height=865");
+        Category category2 = new Category("Keyboards","PC Keyboards", "https://cdn.discordapp.com/attachments/1112238965743964211/1113681140931362938/Allair_Gaming_keyboard_black_color_white_lights_dark_blue_backg_74961f9a-a214-4327-a118-4a30063f06ae.png");
+        Category category3 = new Category("Headphones","PC Headphones", "https://media.discordapp.net/attachments/1112238965743964211/1113832002462949386/Allair_Gaming_headphones_black_color_white_lights_dark_blue_bac_8e006651-9ed9-4bbe-9b19-5281af57ed97.png?width=1298&height=865");
 
         categoryRepository.save(category1);
         categoryRepository.save(category2);
         categoryRepository.save(category3);
 
         // Seed products
-        Product product1 = new Product("Logitech Mouse", "A mouse made by Logitech", 200.0, "https://images-na.ssl-images-amazon.com/images/I/61rk-0XJhPL._SL1500_.jpg", category1);
-        Product product2 = new Product("Logitech Keyboard", "A keyboard made by Logitech", 200.0, "https://images-na.ssl-images-amazon.com/images/I/61rk-0XJhPL._SL1500_.jpg", category2);
-        Product product3 = new Product("Logitech Headphones", "A headphones made by Logitech", 200.0, "https://images-na.ssl-images-amazon.com/images/I/61rk-0XJhPL._SL1500_.jpg", category3);
+        Product product1 = new Product("Zinmouse 8080", "Flagship mice made by Zinware", 200.0, "https://media.discordapp.net/attachments/1112238965743964211/1113833580687274054/Allair_single_gaming_mouse_black_color_white_lights_dark_blue_b_e093442e-5e23-479a-b3a3-07f7160d5cc7.png?width=1298&height=865", category1);
+        Product product2 = new Product("Zinboard 4200", "Flagship keyboard made by Zinware", 200.0, "https://cdn.discordapp.com/attachments/1112238965743964211/1113681140931362938/Allair_Gaming_keyboard_black_color_white_lights_dark_blue_backg_74961f9a-a214-4327-a118-4a30063f06ae.png", category2);
+        Product product3 = new Product("Zinphones 4", "Flagship headphones made by Zinware", 200.0, "https://media.discordapp.net/attachments/1112238965743964211/1113832002462949386/Allair_Gaming_headphones_black_color_white_lights_dark_blue_bac_8e006651-9ed9-4bbe-9b19-5281af57ed97.png?width=1298&height=865", category3);
         Product product4 = new Product("Razer Mouse", "A mouse made by Razer", 200.0, "https://images-na.ssl-images-amazon.com/images/I/61rk-0XJhPL._SL1500_.jpg", category1);
         Product product5 = new Product("Razer Keyboard", "A keyboard made by Razer", 200.0, "https://images-na.ssl-images-amazon.com/images/I/61rk-0XJhPL._SL1500_.jpg", category2);
         Product product6 = new Product("Razer Headphones", "A headphones made by Razer", 200.0, "https://images-na.ssl-images-amazon.com/images/I/61rk-0XJhPL._SL1500_.jpg", category3);
