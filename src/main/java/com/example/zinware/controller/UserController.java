cb2077785a1c23b7510c23bf8a39dd98d8e4d905
Maksym Zinchenko <maklaut98@gmail.com>
2023-06-01 17:18:25 -0500

    Created user controller

---
+++ src/main/java/com/example/zinware/controller/UserController.java
@@ -0,0 +1,16 @@
+package com.example.zinware.controller;
+
+import com.example.zinware.model.User;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(path = "/auth/")
+public class UserController {
+
+
+
+}
