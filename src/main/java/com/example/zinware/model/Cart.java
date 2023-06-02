e3efbd37e51587dd7b98ed7b7253dd9ea44c5a9e
Maksym Zinchenko <maklaut98@gmail.com>
2023-06-02 10:56:01 -0500

    Generated getters and setters for cart

--- src/main/java/com/example/zinware/model/Cart.java
+++ src/main/java/com/example/zinware/model/Cart.java
@@ -17,4 +17,36 @@ public class Cart {
     @OneToOne(cascade = CascadeType.ALL)
     @JoinColumn(name = "user_id", referencedColumnName = "id")
     private User user;
+
+    public Cart(List<CartItem> cartItems, User user) {
+        this.cartItems = cartItems;
+        this.user = user;
+    }
+
+    public Cart() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public List<CartItem> getCartItems() {
+        return cartItems;
+    }
+
+    public void setCartItems(List<CartItem> cartItems) {
+        this.cartItems = cartItems;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
 }
