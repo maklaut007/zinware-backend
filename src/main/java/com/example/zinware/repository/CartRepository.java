33b8571616318b4de9a272377e234de3bf749dd1
Maksym Zinchenko <maklaut98@gmail.com>
2023-06-02 11:45:15 -0500

    Added find cart by user Id in repository

--- src/main/java/com/example/zinware/repository/CartRepository.java
+++ src/main/java/com/example/zinware/repository/CartRepository.java
@@ -3,5 +3,9 @@ package com.example.zinware.repository;
 import com.example.zinware.model.Cart;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+
+import java.util.Optional;
+
 public interface CartRepository extends JpaRepository<Cart,  Long> {
+    Optional<Cart> findByUserId(Long userId);
 }
